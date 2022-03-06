c3f2eca4507ee509b09aa9d4e9dc72de69ccc85d
Jahangir <jahangir117011@gmail.com>
2022-03-06 15:25:43 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import newLogo from './undraw_new_year_2022_bxec.svg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={newLogo} className="App-logo" alt="logo" />
+        <p>
+          {/* Do not Edit <code>src/App.js</code> and save to reload. */}
+          Happy New Year 2022
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
